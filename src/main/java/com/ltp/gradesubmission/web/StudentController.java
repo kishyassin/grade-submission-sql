53b2eada75e281e9d09a64ed5c0a9ba57435b6f1
KICH Yassine <kishyassin@gmail.com>
2024-09-11 18:56:33 +0100

    cofigure end point for requests on /student

--- src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -3,9 +3,43 @@ package com.ltp.gradesubmission.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ltp.gradesubmission.entity.Student;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
+    @PostMapping
+    public ResponseEntity<Student> saveStudent (@RequestBody Student student) {
+        return new ResponseEntity<>(student,HttpStatus.CREATED);    
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable long id){
+        return new ResponseEntity<>(HttpStatus.valueOf(204));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
 }
